ae7e5421f3e0f09107a755d9816e926c8e15d042
Andrew Alteri <andrewsalteri@gmail.com>
2020-07-20 15:02:51 -0500

    Done.

--- config/environment.rb
+++ config/environment.rb
@@ -2,7 +2,9 @@ ENV['SINATRA_ENV'] ||= "development"
 
 require 'bundler/setup'
 Bundler.require(:default, ENV['SINATRA_ENV'])
+require 'securerandom'
 
 ActiveRecord::Base.establish_connection(ENV['SINATRA_ENV'].to_sym)
 
+require './app/controllers/application_controller'
 require_all 'app'
