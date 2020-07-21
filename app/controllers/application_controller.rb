6d42609d11cc6a0a5f346d02407cfd919adfd177
Dice-K39 <daisukehashimoto39@gmail.com>
2020-07-20 23:42:52 -0400

    Rework

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,14 @@
 require './config/environment'
 
 class ApplicationController < Sinatra::Base
-
   configure do
     set :public_folder, 'public'
     set :views, 'app/views'
+    enable :sessions
+    set :session_secret, "CHANGE ME NAO!"
   end
 
+  get '/' do
+    erb :"index"
+  end
 end
