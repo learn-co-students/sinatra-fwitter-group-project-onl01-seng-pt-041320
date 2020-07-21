8bc1383a50dbefccbff6732d63de719ef5c0f855
Dice-K39 <daisukehashimoto39@gmail.com>
2020-07-20 23:14:29 -0400

    Done.

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,95 +1,84 @@
 class TweetsController < ApplicationController
     get "/tweets" do
         if !Helpers.is_logged_in?(session)
-            redirect "/login"
+            redirect to "/login"
         else
             @tweets = Tweet.all
-            
+            @user = Helpers.current_user(session)
+
             erb :"tweets/tweets"
         end
     end
 
     get "/tweets/new" do
         if !Helpers.is_logged_in?(session)
-            redirect "/login"
-        else
-            erb :"tweets/create_tweet"
+            redirect to "/login"
         end
+
+        erb :"tweets/create_tweet"
     end
 
     post "/tweets" do
-        if Helpers.is_logged_in?(session)
-            if params[:content] == ""
-                redirect "/tweets/new"
-            else
-                tweet = current_user.tweets.build(content: params[:content])
-                
-                if tweet.save
-                    redirect "/tweets/#{tweet.id}"
-                else
-                    redirect "/tweets/new"
-                end
-            end
-        else
-            redirect "/login"
+        user = Helpers.current_user(session)
+
+        if params[:content].empty?
+            redirect to "/tweets/new"
         end
+        tweet = Tweet.create(:content => params[:content], :user_id => user.id)
+
+        redirect to "/tweets"       
     end
 
     get "/tweets/:id" do
         if Helpers.is_logged_in?(session)
-          @tweet = Tweet.find_by_id(params[:id])
-          erb :"tweets/show_tweet"
+            @tweet = Tweet.find_by_id(params[:id])
+            
+            erb :"tweets/show_tweet"
         else
           redirect to "/login"
         end
     end
 
     get "/tweets/:id/edit" do
-        if Helpers.is_logged_in?(session)
-            @tweet = Tweet.find_by_id(params[:id])
-
-            if @tweet && @tweet.user == current_user
-                erb :"tweets/edit_tweet"
-            else
-                redirect to "/tweets"
-            end
-        else
+        if !Helpers.is_logged_in?(session)
             redirect to "/login"
         end
+
+        @tweet = Tweet.find_by_id(params[:id])
+
+        if Helpers.current_user(session).id != @tweet.user_id
+            redirect to "/tweets"
+        end
+
+        erb :"tweets/edit_tweet"
     end
 
     patch "/tweets/:id" do
-        if Helpers.is_logged_in?(session)
-            if params[:content] == ""
-                redirect to "/tweets/#{params[:id]}/edit"
-            else
-                @tweet = Tweet.find_by_id(params[:id])
+        tweet = Tweet.find_by_id(params[:id])
 
-                if @tweet && @tweet.user == current_user
-                    if @tweet.update(content: params[:content])
-                        redirect to "/tweets/#{@tweet.id}"
-                    else
-                        redirect to "/tweets/#{@tweet.id}/edit"
-                    end
-                else
-                    redirect to "/tweets"
-                end
-            end
-        else
-            redirect to "/login"
+        if params[:content].empty?
+            redirect to "/tweets/#{params[:id]}/edit"
         end
+        tweet.update(:content => params[:content])
+
+        tweet.save
+
+        redirect to "/tweets/#{tweet.id}"
     end
 
     delete "/tweets/:id/delete" do
-        if Helpers.is_logged_in?(session)
-            @tweet = Tweet.find_by_id(params[:id])
-
-            if @tweet && @tweet.user == current_user
-                @tweet.delete
-            end
-            redirect to "/tweets"
-        else
+        if !Helpers.is_logged_in?(session)
             redirect to "/login"
         end
+
+        @tweet = Tweet.find_by_id(params[:id])
+
+        if Helpers.current_user(session).id != @tweet.user_id
+            redirect to "/tweets"
+        end
+
+        @tweet.delete
+
+        redirect to "/tweets"
     end
 end
