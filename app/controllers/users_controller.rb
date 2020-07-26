7e5f5a7a78ebd986e73d800605606343137e39d1
marcusLau <lau.marcus76@gmail.com>
2020-07-26 14:11:31 -0700

    Done.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,28 @@
 class UsersController < ApplicationController
 
+    get '/signup' do 
+        if logged_in?
+            redirect to '/tweets'
+        else
+            erb :"/users/new"
+        end
+    end
+
+    post '/signup' do 
+        @user = User.create(
+            username: params[:username],
+            email: params[:email],
+            password: params[:password])
+        @user.save
+        redirect '/tweets/#{@user.id}'
+    end
+
+    get '/login' do
+        if logged_in?
+          redirect to '/tweets'
+        else
+          erb :"/users/login"
+        end
+      end
 
 end
